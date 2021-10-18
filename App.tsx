7f6fb66ad29d32e3c9f8c4e6d1a9170979575c95
tulpardev <suha.arikan@icloud.com>
2021-10-19 00:56:29 +0300

    series poster added

--- App.tsx
+++ App.tsx
@@ -1,48 +1,318 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import * as React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
-  useColorScheme,
+  Image,
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
   View,
+  StyleSheet,
+  SafeAreaView,
 } from 'react-native';
+const {width} = Dimensions.get('screen');
+import {EvilIcons} from '@expo/vector-icons';
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from 'react-native-gesture-handler';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+// https://www.creative-flyers.com
+const DATA = [
+  {
+    title: 'Afro vibes',
+    location: 'Mumbai, India',
+    date: 'Nov 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Afro-vibes-flyer-template.jpg',
+  },
+  {
+    title: 'Jungle Party',
+    location: 'Unknown',
+    date: 'Sept 3rd, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2019/11/Jungle-Party-Flyer-Template-1.jpg',
+  },
+  {
+    title: '4th Of July',
+    location: 'New York, USA',
+    date: 'Oct 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/4th-Of-July-Invitation.jpg',
+  },
+  {
+    title: 'Summer festival',
+    location: 'Bucharest, Romania',
+    date: 'Aug 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Summer-Music-Festival-Poster.jpg',
+  },
+  {
+    title: 'BBQ with friends',
+    location: 'Prague, Czech Republic',
+    date: 'Sept 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/BBQ-Flyer-Psd-Template.jpg',
+  },
+  {
+    title: 'Festival music',
+    location: 'Berlin, Germany',
+    date: 'Apr 21th, 2021',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Festival-Music-PSD-Template.jpg',
+  },
+  {
+    title: 'Beach House',
+    location: 'Liboa, Portugal',
+    date: 'Aug 12th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Summer-Beach-House-Flyer.jpg',
+  },
+];
 
-import Words from './src/Words';
+const OVERFLOW_HEIGHT = 70;
+const SPACING = 10;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const VISIBLE_ITEMS = 3;
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+interface OverflowItemsPropsType {
+  data: any;
+  scrollXAnimated: any;
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const OverflowItems: React.FC<OverflowItemsPropsType> = ({
+  data,
+  scrollXAnimated,
+}) => {
+  const inputRange = [-1, 0, 1];
+  const translateY = scrollXAnimated.interpolate({
+    inputRange,
+    outputRange: [OVERFLOW_HEIGHT, 0, -OVERFLOW_HEIGHT],
+  });
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Words name="Süha" />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.overflowContainer}>
+      <Animated.View style={{transform: [{translateY}]}}>
+        {data.map(
+          (
+            item: {
+              title:
+                | boolean
+                | React.ReactChild
+                | React.ReactFragment
+                | React.ReactPortal
+                | null
+                | undefined;
+              location:
+                | string
+                | number
+                | boolean
+                | {}
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | React.ReactNodeArray
+                | React.ReactPortal
+                | null
+                | undefined;
+              date:
+                | boolean
+                | React.ReactChild
+                | React.ReactFragment
+                | React.ReactPortal
+                | null
+                | undefined;
+            },
+            index: React.Key | null | undefined,
+          ) => {
+            return (
+              <View key={index} style={styles.itemContainer}>
+                <Text style={[styles.title]} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <View style={styles.itemContainerRow}>
+                  <Text style={[styles.location]}>
+                    <EvilIcons
+                      name="location"
+                      size={16}
+                      color="black"
+                      style={{marginRight: 5}}
+                    />
+                    {item.location}
+                  </Text>
+                  <Text style={[styles.date]}>{item.date}</Text>
+                </View>
+              </View>
+            );
+          },
+        )}
+      </Animated.View>
+    </View>
   );
 };
 
-export default App;
+export default function App() {
+  const [data, setData] = React.useState(DATA);
+  const scrollXIndex = React.useRef(new Animated.Value(0)).current;
+  const scrollXAnimated = React.useRef(new Animated.Value(0)).current;
+  const [index, setIndex] = React.useState(0);
+  const setActiveIndex = React.useCallback(
+    activeIndex => {
+      scrollXIndex.setValue(activeIndex);
+      setIndex(activeIndex);
+    },
+    [scrollXIndex],
+  );
+
+  React.useEffect(() => {
+    if (index === data.length - VISIBLE_ITEMS - 1) {
+      // get new data
+      // fetch more data
+      const newData = [...data, ...data];
+      setData(newData);
+    }
+  }, [index, data]);
+
+  React.useEffect(() => {
+    Animated.spring(scrollXAnimated, {
+      toValue: scrollXIndex,
+      useNativeDriver: true,
+    }).start();
+  });
+
+  return (
+    <FlingGestureHandler
+      key="left"
+      direction={Directions.LEFT}
+      onHandlerStateChange={ev => {
+        if (ev.nativeEvent.state === State.END) {
+          if (index === data.length - 1) {
+            return;
+          }
+          setActiveIndex(index + 1);
+        }
+      }}>
+      <FlingGestureHandler
+        key="right"
+        direction={Directions.RIGHT}
+        onHandlerStateChange={ev => {
+          if (ev.nativeEvent.state === State.END) {
+            if (index === 0) {
+              return;
+            }
+            setActiveIndex(index - 1);
+          }
+        }}>
+        <SafeAreaView style={styles.container}>
+          <StatusBar hidden />
+          <OverflowItems data={data} scrollXAnimated={scrollXAnimated} />
+          <FlatList
+            data={data}
+            keyExtractor={(_, index) => String(index)}
+            horizontal
+            inverted
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: 'center',
+              padding: SPACING * 2,
+              marginTop: 50,
+            }}
+            scrollEnabled={false}
+            removeClippedSubviews={false}
+            CellRendererComponent={({
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              item,
+              index,
+              children,
+              style,
+              ...props
+            }) => {
+              const newStyle = [style, {zIndex: data.length - index}];
+              return (
+                <View style={newStyle} index={index} {...props}>
+                  {children}
+                </View>
+              );
+            }}
+            renderItem={({item, index}) => {
+              const inputRange = [index - 1, index, index + 1];
+              const translateX = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [50, 0, -100],
+              });
+              const scale = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [0.8, 1, 1.3],
+              });
+              const opacity = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
+              });
+
+              return (
+                <Animated.View
+                  // eslint-disable-next-line react-native/no-inline-styles
+                  style={{
+                    position: 'absolute',
+                    left: -ITEM_WIDTH / 2,
+                    opacity,
+                    transform: [
+                      {
+                        translateX,
+                      },
+                      {scale},
+                    ],
+                  }}>
+                  <Image
+                    source={{uri: item.poster}}
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{
+                      width: ITEM_WIDTH,
+                      height: ITEM_HEIGHT,
+                      borderRadius: 14,
+                    }}
+                  />
+                </Animated.View>
+              );
+            }}
+          />
+        </SafeAreaView>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    letterSpacing: -1,
+  },
+  location: {
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 12,
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    padding: SPACING * 2,
+  },
+  itemContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  overflowContainer: {
+    height: OVERFLOW_HEIGHT,
+    overflow: 'hidden',
+  },
+});
